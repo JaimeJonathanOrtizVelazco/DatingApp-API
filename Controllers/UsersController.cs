c170b7c4024be3b20d6b6d522fd639718d1419e0
Jonathan Velazco <jonathan.ortiz.velazco@gmail.com>
2020-05-19 23:01:55 -0500

    Updating userController

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AutoMapper;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class UsersController : ControllerBase
+    {
+        private readonly IDatingRepository repository;
+        private readonly IMapper mapper;
+        public UsersController(IDatingRepository repository, IMapper mapper)
+        {
+            this.mapper = mapper;
+            this.repository = repository;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await repository.GetUsers();
+            var usersToReturn= mapper.Map<IEnumerable<UserForListDto>>(users);
+            return Ok(usersToReturn);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            var user = await repository.GetUser(id);
+            var userToReturn= mapper.Map<UserForDetailsDto>(user);
+            return Ok(userToReturn);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, UserForUpdateDto userForUpdateDto){
+            if(id!= int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value)){
+                return Unauthorized();
+            }
+            var userFromRepo= await repository.GetUser(id);
+            mapper.Map(userForUpdateDto,userFromRepo);
+            if(await repository.SaveAll()){
+                return NoContent();
+            }
+            throw new Exception($"Updating user {id} failed on save");
+        }
+    }
+}
